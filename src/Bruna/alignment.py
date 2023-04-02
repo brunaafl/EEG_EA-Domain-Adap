ac91343053d5b9323f2ca178313151e9a658abfa
brunaafl <brunaafl@usp.br>
2023-04-02 15:51:50 -0300

    minor adap if already aligned

--- src/Bruna/alignment.py
+++ src/Bruna/alignment.py
@@ -19,23 +19,29 @@ def euclidean_alignment(data, y=None):
 
     r = r / len(data)
 
-    if iscomplexobj(r):
-        print("covariance matrix problem")
-    if iscomplexobj(sqrtm(r)):
-        print("covariance matrix problem sqrt")
+    compare = np.allclose(r, np.identity(r.shape[0]))
 
-    r_op = inv(sqrtm(r))
+    if not compare:
 
-    if iscomplexobj(r_op):
-        print(
-            "WARNING! Covariance matrix was not SPD somehow. "
-            + "Can be caused by running ICA-EOG rejection, if "
-            + "not, check data!!"
-        )
-        r_op = real(r_op).astype(np.float64)
-    elif not any(isfinite(r_op)):
-        print("WARNING! Not finite values in R Matrix")
+        if iscomplexobj(r):
+            print("covariance matrix problem")
+        if iscomplexobj(sqrtm(r)):
+            print("covariance matrix problem sqrt")
 
-    result = np.matmul(r_op, data)
+        r_op = inv(sqrtm(r))
+
+        if iscomplexobj(r_op):
+            print("WARNING! Covariance matrix was not SPD somehow. " +
+                  "Can be caused by running ICA-EOG rejection, if " +
+                  "not, check data!!")
+            r_op = real(r_op).astype(np.float64)
+        elif not any(isfinite(r_op)):
+            print("WARNING! Not finite values in R Matrix")
+
+        result = np.matmul(r_op, data)
+
+    else:
+        print("Already aligned!")
+        result = data
 
     return result
