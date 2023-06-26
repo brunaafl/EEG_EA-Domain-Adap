f8723648ed1bb368b1a706daeef0f16a403d8665
brunaafl <brunaafl@usp.br>
2023-06-26 08:16:34 -0300

    adjusting patience

--- src/Bruna/evaluation.py
+++ src/Bruna/evaluation.py
@@ -633,7 +633,7 @@ def online_indiv(dataset, paradigm, pipes, nn_model, run_dir):
         # iterate over each pipeline
         for name, clf in pipes.items():
 
-            ftclf = create_clf_ft(nn_model, 600, optimizer__lr=0.0625 * 0.01, optimizer__weight_decay=1e-5, batch_size=64)
+            ftclf = create_clf_ft(nn_model, 600, optimizer__lr=0.0625 * 0.01, optimizer__weight_decay=1e-5, batch_size=8)
             ftclf.initialize()
 
             # Initialize with the saved parameters
