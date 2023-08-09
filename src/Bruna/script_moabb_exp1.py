211278d80bbe3eb48504c2e305b1446fc9ef3691
brunaafl <brunaafl@usp.br>
2023-08-09 19:24:26 -0300

    Schirrmeister scripts adaptation

--- src/Bruna/script_moabb_exp1.py
+++ src/Bruna/script_moabb_exp1.py
@@ -66,15 +66,6 @@ def main(args):
     X, labels, meta = paradigm.get_data(dataset=dataset, subjects=[1])
     n_chans = X.shape[1]
     input_window_samples = X.shape[2]
-
-    """
-    runs = meta.run.values
-    sessions = meta.session.values
-    one_session = sessions == np.unique(sessions)[0]
-    one_run = runs == np.unique(runs)[0]
-    run_session = np.logical_and(one_session, one_run)
-    len_run = sum(run_session * 1)"""
-
     ea = config.ea.batch
 
     model = init_model(n_chans, n_classes, input_window_samples, config=config)
