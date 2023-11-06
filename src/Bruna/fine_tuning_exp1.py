9cfed8ea8701dce9f4c570db4c5f9b2e94459cbc
brunalopes <brunaafl@usp.br>
2023-11-06 16:01:08 +0100

    fine-tuning shared models

--- src/Bruna/fine_tuning_exp1.py
+++ src/Bruna/fine_tuning_exp1.py
@@ -87,24 +87,24 @@ def main(args):
     if args.ea == 'alignment':
         pipes[f"{config.model.type}_EA"] = pipe_with_align
         if config.model.type == "EEGNetv4":
-            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/run/EpochsFalse/shared_m1_final-{args.dataset}-alignment' \
+            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/runs/EpochsFalse/shared_m1_final-{args.dataset}-alignment' \
                   '-exp_1-0-both'
         elif config.model.type == "ShallowFBCSPNet":
-            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/run/EpochsFalse/shared_m3_final-{args.dataset}-alignment' \
+            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/runs/EpochsFalse/shared_m3_final-{args.dataset}-alignment' \
                   '-exp_1-0-both'
         elif config.model.type == "Deep4Net":
-            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/run/EpochsFalse/shared_m2_final-{args.dataset}-alignment' \
+            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/runs/EpochsFalse/shared_m2_final-{args.dataset}-alignment' \
                   '-exp_1-0-both'
     else:
         pipes[f"{config.model.type}_Without_EA"] = pipe
         if config.model.type == "EEGNetv4":
-            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/run/EpochsFalse/shared_m1_final-{args.dataset}-no' \
+            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/runs/EpochsFalse/shared_m1_final-{args.dataset}-no' \
                   '-alignment-exp_1-0-both'
         elif config.model.type == "ShallowFBCSPNet":
-            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/run/EpochsFalse/shared_m3_final-{args.dataset}-alignment' \
+            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/runs/EpochsFalse/shared_m3_final-{args.dataset}-alignment' \
                   '-exp_1-0-both'
         elif config.model.type == "Deep4Net":
-            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/run/shared_m2_final-{args.dataset}-alignment' \
+            run = f'/mnt/beegfs/home/aristimunha/bruna/EEG_EA-Domain-Adap/output/runs/shared_m2_final-{args.dataset}-alignment' \
                   '-exp_1-0-both'
     # Now, Online with 1 run for EA and ft
     results = online_shared(dataset, paradigm, pipes, model, Path(run), config)
